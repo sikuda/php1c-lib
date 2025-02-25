08f3fb4cf868e9b145c9e2fa05277159d852068c
Sergey Kudashkin <ya.sikuda@yandex.ru>
2025-02-25 07:32:32 +0300

    makeCode with nlibrary

--- src/CodeStream.php
+++ src/CodeStream.php
@@ -764,54 +764,44 @@ class CodeStream {
 	// *
 	// * @param string $buffer строка код для преобразования
 	// */
-	// function makeCode(string $buffer, $name_var=null){
+	function makeCode(string $buffer, $name_var=null){
 
-	// 	$tokenStream = new TokenStream($buffer);
-	// 	$resToken = $tokenStream->CodeToTokens();
-	// 	if ($resToken !== true) {
-	// 		return $resToken; //возврат ошибки разбора
-	// 	}
-	// 	$this->functions1C = $tokenStream->functions1C;
-	// 	$this->keywords = $tokenStream->idTypes;
-	// 	$this->tokens = $tokenStream->tokens;
+		$tokenStream = new TokenStream($buffer);
+		$resToken = $tokenStream->CodeToTokens();
+		if ($resToken !== true) {
+			return $resToken; //возврат ошибки разбора
+		}
+		$this->functions1C = $tokenStream->functions1C;
+		$this->keywords = $tokenStream->idTypes;
+		$this->tokens = $tokenStream->tokens;
 
-	// 	//var_dump($this->tokens);
-	// 	//return "";
-		
-	// 	//Блок преобразования в код php
-	// 	try{
-	// 		$this->code = '';
-	// 		$this->codePHP = '';
-	// 		$this->GetChar();
-	// 		if($this->Type !== TokenStream::type_end_code){
+		//var_dump($this->tokens);
+		//return "";
+	   
+		//Блок преобразования в код php
+		try{
+			$this->code = '';
+			$this->codePHP = '';
+			$this->GetChar();
+			if($this->Type !== TokenStream::type_end_code){
 
-	// 			$this->continueCode();
+				$this->continueCode();
 
-	// 			//Вывод результата переменной
-	// 			if(isset($name_var)){
-	// 				if(fEnglishVariable) $name_var = str_replace(php1C_LetterLng, php1C_LetterEng, $name_var);
-	// 				$name_var = mb_strtoupper($name_var);
-    //                 eval($this->codePHP);
-    //                 return ${$name_var};
-	// 			}
-	// 			else return $this->codePHP;
-	// 		}  
-	// 		else return ""; //стиль 1С нет ошибки
-	// 	}
-	// 	catch (Exception $e) {
-	// 		$token = $this->tokens[$this->i_token-1];
-    // 		return (" {(".$token->row.",".$token->col.")}: ".$e->getMessage()); //стиль ошибки 1С
-	// 	}
- 	// }
+				//Вывод результата переменной
+				if(isset($name_var)){
+					if(fEnglishVariable) $name_var = str_replace(php1C_LetterLng, php1C_LetterEng, $name_var);
+					$name_var = mb_strtoupper($name_var);
+					eval($this->codePHP);
+					return ${$name_var};
+				}
+				else return $this->codePHP;
+			}
+			else return ""; //стиль 1С нет ошибки
+		}
+		catch (Exception $e) {
+			$token = $this->tokens[$this->i_token-1];
+			return (" {(".$token->row.",".$token->col.")}: ".$e->getMessage()); //стиль ошибки 1С
+		}
+	 }
 }
 
-// /**
-// * Запуск получения кода PHP
-// *
-// * @param string $buffer строка код для преобразования
-// * @param string|null $name_var имя переменной для вывода результата выполнения кода
-// */
-// function makeCode(string $buffer, string $name_var = null){
-// 	$stream = new CodeStream();
-//     return $stream->makeCode($buffer, $name_var);
-// }
